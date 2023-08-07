4894c2eaf6180890f19912404d421bbd5f1a3893
Soul-8789 <102282026+Soul-8789@users.noreply.github.com>
2023-08-07 15:02:29 +0530

    commit

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const app = express(); //Initiliazed and server ready
 
 app.use(express.static("public"));
 
-let port = process.env.PORT||5000;
+let port = process.env.PORT||3000;
+
 //port
 let server = app.listen(port, () => {
     console.log("Listening to port " + port);
